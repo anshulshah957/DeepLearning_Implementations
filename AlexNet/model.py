bf69e1bf92f494750df78428a257717cac038442
Anshul <anshuls3@illinois.edu>
2020-08-07 20:25:57 -0500

    Initial model interface

---
+++ AlexNet/model.py
@@ -0,0 +1,33 @@
+'''
+AlexNet Implementation from:
+"ImageNet Classification with Deep Convolutional Neural Networks" by Krizhevsky et al.
+https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+'''
+
+#TODO: Add imports
+
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class AlexNet(nn.module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def train(self):
+        pass
+
+    def load_weights(self):
+        pass
+
+    def train(self):
+        pass
+
+if __name__ == "__main__":
+    pass
+
+
