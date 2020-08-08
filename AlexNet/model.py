7e27e8b979791f612ec4e4f37611fbd599f83f9d
Anshul <anshuls3@illinois.edu>
2020-08-07 23:47:03 -0500

    Added some data loading and model layers

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -12,11 +12,55 @@ import torch.nn as nn
 import torch.optim as optim
 
 class AlexNet(nn.module):
-    def __init__(self):
-        pass
+    #TODO: Calculate correct conv_output_size
+    def __init__(self, image_width=60, image_height=60, image_channels=3, num_classes=10, conv_output_size=(6 * 6 * 256)):
+        super.__init__()
 
-    def forward(self):
-        pass
+        self.input_width = image_width
+        self.input_height = image_height
+        self.input_channels = image_channels
+        self.num_classes = num_classes
+        self.conv_output_size = conv_output_size
+
+        self.conv = nn.Sequential(
+                nn.Conv2d(in_channels=self.input_channels, out_channels=96, kernel_size=11, stride=4),
+                nn.ReLU(),
+                nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.FC = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=conv_output_size, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=num_classes),
+        )
+
+        #TODO: Initialize biases
+
+
+
+
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, self.conv_output_size)
+        x = self.FC(x)
+        
+        return x
 
     def train(self):
         pass
@@ -24,7 +68,7 @@ class AlexNet(nn.module):
     def load_weights(self):
         pass
 
-    def train(self):
+    def train(self, learning_rate=0.01, momentum=0.9):
         pass
 
 if __name__ == "__main__":
