6be498afb9ddab35814b563a35ba22e9859814ac
Anshul <anshuls3@illinois.edu>
2020-08-09 11:11:38 -0500

    Implemented training function

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -7,22 +7,24 @@ https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutiona
 #TODO: Add imports
 
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
+from torch.utils import data
 from load_data import get_cifar_10
+from torch.nn.functional import cross_entropy
 
-class AlexNet(nn.module):
+class AlexNet(nn.Module):
     #TODO: Calculate correct conv_output_size
     def __init__(self, image_width=60, image_height=60, image_channels=3, num_classes=10):
-        super.__init__()
+        super().__init__()
 
         self.input_width = image_width
         self.input_height = image_height
         self.input_channels = image_channels
         self.num_classes = num_classes
-        self.conv_output_size = conv_output_size
 
         self.conv = nn.Sequential(
                 nn.Conv2d(in_channels=self.input_channels, out_channels=96, kernel_size=11, stride=4),
@@ -42,8 +44,13 @@ class AlexNet(nn.module):
                 nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
-        x = np.zeroes((self.input_width, self.input_height))
+        x = np.zeros((3, self.input_width, self.input_height))
+        x = torch.Tensor(x)
+        x = x.unsqueeze(0)
+
         conv_output_size = self.conv(x).numel()
+
+        self.conv_output_size = conv_output_size
 
         self.FC = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
@@ -56,7 +63,7 @@ class AlexNet(nn.module):
         )
 
         '''
-        Weight initializations according to paper
+        Weight and Bias initializations according to paper
         '''
 
         nn.init.normal_(self.conv[0].weight, mean=0, std=0.01)
@@ -89,31 +96,48 @@ class AlexNet(nn.module):
     def load_weights(self):
         pass
 
-    def load_dataset(self, X_train, Y_train, X_test, Y_test):
-        self.train_dataset = TensorDataset((X_train, Y_train))
-        self.test_dataset = TensorDataset((X_test, Y_test))
+    def load_dataset(self, X_train, Y_train, X_test, Y_test, batch_size=128):
+        #TODO: add data augmentation
+        train_dataset = TensorDataset((X_train, Y_train))
+        test_dataset = TensorDataset((X_test, Y_test))
+
+        self.train_dataloader = data.DataLoader(train_dataset, shuffle=True, pin_memory=True, num_workers=8, drop_last=True, batch_size=batch_size)
+        self.test_dataloader = data.DataLoader(test_dataset, shuffle=True, pin_memory=True, num_workers=8, drop_last=True, batch_size=batch_size)
+
  
     #TODO: Use multiple GPUs
-    def train(self, device, learning_rate=0.01, learning_momentum=0.9, learning_decay=0005, num_epochs=90, batch_size=128):
+    def train(self, device, learning_rate=0.01, learning_momentum=0.9, learning_decay=0.0005, num_epochs=90):
         
         optimizer = optim.SGD(params=self.parameters(), lr=learning_rate, momentum=learning_momentum, weight_decay=learning_decay)
         
         #TODO: divide the learning rate by 10 when the validation error rate stops improving with the current learning rate
+        steps = 0
         for epoch in range(0, num_epochs):
-            pass
-        
-        return
+            for imgs, classes in self.train_dataloader:
+                imgs, classes = imgs.to(device), classes.to(device)
 
+                pred = self(imgs)
+                loss = cross_entropy(pred, classes)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+                #Temporary debugging, add log to something like tensorboard later
+                print(loss.item())
+                print(accuracy.item())
 
 if __name__ == "__main__":
-    alexNet = AlexNet()
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+
+    alexNet = AlexNet(70, 70).to(device)
 
     X_train, Y_train, X_test, Y_test = get_cifar_10(70, 70)
 
     alexNet.load_dataset(torch.Tensor(X_train), torch.Tensor(Y_train), torch.Tensor(X_test), torch.Tensor(Y_test))
-
-    device = torch.device('cpu')
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
+    
+    alexNet.train(device)
 
 
